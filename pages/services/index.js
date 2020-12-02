448808ee4803037d0fac732112212fc0d309a18b
Anzor Shogenov <anzor9657@gmail.com>
2020-12-02 23:56:28 +0300

    completed services form rendering

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,26 +1,138 @@
 $(document).ready(() => {
+
     // Elements
     const $search = $('#search');
     const $clearSearchBtn = $('#clearSearchBtn');
     const $tbody = $('#tbody');
     const $langTabs = $('#langTabs');
     const $categorySelect = $('#categorySelect');
-    const $form = $('#form');
-    const $saveBtn = $('#saveBtn');
+    const $visibilitySelect = $('#visibilitySelect');
+    const $propsSelect = $('#propsSelect');
+    const $contentSelect = $('#contentSelect');
+    const $funcsSelect = $('#funcsSelect');
+
+
+    const $form = $('form');
 
     // Variabes
     let serviceId;
-    const formData = {
-        category: 5,
-        name: '',
-        visibilities: [],
-        openIn: '',
-        generalDescription: false,
-        content: [],
-        isCatalog: false,
-        isType: false,
-        icon: null,
+    class FormData {
+        _category
+        _name
+        _visibilities
+        _openIn
+        _generalDescriptionStatus
+        _propertiesStatus
+        _companyStatus
+        _properties
+        _content
+        _functions
+        _icon
+
+        constructor(props) {
+            for (const [key, val] of Object.entries(props)) {
+                this[`_${key}`] = val;
+            }
+        }
+
+        get category() {
+            return this._category;
+        }
+        set category({ value }) {
+            this._category = Number(value);
+            this._content = [];
+            fillContentSelect();
+        }
+        get name() {
+            return this._name;
+        }
+        set name({ value }) {
+            this._name = value;
+        }
+        get visibilities() {
+            return this._visibilities;
+        }
+        set visibilities(target) {
+            this._visibilities = $visibilitySelect.val();
+        }
+        get openIn() {
+            return this._openIn;
+        }
+        set openIn({ value }) {
+            this._openIn = value;
+        }
+        get generalDescriptionStatus() {
+            return this._generalDescriptionStatus;
+        }
+        set generalDescriptionStatus({ checked }) {
+            this._generalDescriptionStatus = checked;
+            if (checked) {
+                $('#editDescription').parent().removeClass('d-none');
+            } else {
+                $('#editDescription').parent().addClass('d-none');
+            }
+        }
+        get propertiesStatus() {
+            return this._propertiesStatus;
+        }
+        set propertiesStatus({ checked }) {
+            this._propertiesStatus = checked;
+            if (checked) {
+                $propsSelect.parent().removeClass('d-none');
+            } else {
+                $propsSelect.parent().addClass('d-none');
+                this._properties = [];
+            }
+        }
+        get companyStatus() {
+            return this._companyStatus;
+        }
+        set companyStatus({ checked }) {
+            this._companyStatus = checked;
+            if (checked) {
+                $funcsSelect.parent().removeClass('d-none');
+            } else {
+                $funcsSelect.parent().addClass('d-none');
+                this._functions = [];
+            }
+        }
+        get _properties() {
+            return this._properties;
+        }
+        set _properties(target) {
+            const value = $propSelect.val()
+            this._properties = value;
+        }
+        get content() {
+            return this._content;
+        }
+        set content(target) {
+            const value = $contentSelect.val()
+            this._content = value;
+        }
+        get _functions() {
+            return this._functions;
+        }
+        set _functions(target) {
+            const value = $funcSelect.val()
+            this._functions = value;
+        }
+        get isCatalog() {
+            return this._isCatalog;
+        }
+        set isCatalog({ checked }) {
+            this._isCatalog = checked;
+        }
+        get icon() {
+            return this._icon;
+        }
+        set icon({ files }) {
+            this._icon = files[0];
+            $('label[for=iconInput').text(this._icon.name);
+            fileReader(this._icon);
+        }
     }
+    const formData = new FormData({ category: 3 });
     const categories = [{
             id: 1,
             name: 'Ожидание',
@@ -52,6 +164,10 @@ $(document).ready(() => {
             name: 'Инфраструктура',
         },
     ];
+    const serviceGroups = [
+        { id: 1, name: 'Кафе и рестораны' },
+        { id: 2, name: 'Магазины' },
+    ]
     const services = [
         { id: 1, name: "COVID", categoryId: 1, visibilities: [1, 2] },
         { id: 2, name: "Fast Track", categoryId: 1, visibilities: [1, 2] },
@@ -72,6 +188,13 @@ $(document).ready(() => {
         { id: 14, name: "Камера хранения", categoryId: 2, visibilities: [1, 2] },
         { id: 15, name: "Багажные тележки", categoryId: 2, visibilities: [1, 2] },
         { id: 16, name: "Бюро находок", categoryId: 2, visibilities: [1, 2] },
+        { id: 17, name: "Кафе", groupId: 1, categoryId: 3, visibilities: [1, 2] },
+        { id: 18, name: "Фастфуд", groupId: 1, categoryId: 3, visibilities: [1, 2] },
+        { id: 19, name: "Аптеки", groupId: 2, categoryId: 3, visibilities: [1, 2] },
+        { id: 20, name: "Сувениры", groupId: 2, categoryId: 3, visibilities: [1, 2] },
+        { id: 21, name: "Цветы", groupId: 2, categoryId: 3, visibilities: [1, 2] },
+        { id: 22, name: "Заказ с доставкой", categoryId: 3, visibilities: [1, 2] },
+        { id: 23, name: "Обмен валют", categoryId: 3, visibilities: [1, 2] },
     ];
     const visibilities = [{
             id: 1,
@@ -90,6 +213,8 @@ $(document).ready(() => {
         fillTable(categories);
         renderTabs();
         fillCategoriesSelect();
+        fillVisibilitiesSelect();
+        fillContentSelect();
     }
 
     function setContentHeader() {
@@ -103,7 +228,7 @@ $(document).ready(() => {
                     textToRender = row.name;
                 }
             } else {
-                textToRender = ' - Добавить';
+                textToRender = 'Добавить';
             }
         };
         if (textToRender) {
@@ -166,34 +291,92 @@ $(document).ready(() => {
         $langTabs.append(tabs.join(''));
     }
 
+    function fileReader(file) {
+        if (file) {
+            fr = new FileReader();
+            fr.onload = function(e) {
+                $('#iconPreview').attr('src', e.target.result);
+                $('#iconPreview').removeClass('d-none');
+            }
+            fr.readAsDataURL(file);
+        } else {
+            $('#iconPreview').addClass('d-none');
+        }
+    }
+
     function fillCategoriesSelect() {
         $categorySelect.empty();
         let renderOptions = '';
         for (const category of categories) {
-            renderOptions += `<option value="category-${category.id}" class="${category.id === formData.category ? 'active' : ''}">${category.name}</option>`;
+            renderOptions += `<option value="${category.id}">${category.name}</option>`;
         }
         $categorySelect.append(renderOptions);
-        $categorySelect.val(`category-${formData.category}`);
+        $categorySelect.val(formData.category);
     }
 
-    function formInputHandler(event) {
-        const { target: { name, value } } = event;
-        switch (name) {
-            case 'category':
-                const [, id] = value.split('-');
-                formData[name] = Number(id);
-                break;
-            case 'publishInApp':
-                formData[name] = event.target.checked;
-                break;
-            default:
-                formData[name] = value;
+    function fillVisibilitiesSelect() {
+        $visibilitySelect.empty();
+        let renderOptions = '';
+        for (const visibility of visibilities) {
+            renderOptions += `<option value="${visibility.id}">${visibility.name}</option>`;
+        }
+        $visibilitySelect.append(renderOptions);
+        $visibilitySelect.val(formData.visibilities);
+    }
+
+    function fillContentSelect() {
+        $contentSelect.empty();
+        const selectedCategory = formData.category ? categories.find((category) => category.id === formData.category) : null;
+        let rowToRender = '';
+        let lastGroupId = null;
+        for (const service of services) {
+            if (selectedCategory) {
+                if (service.categoryId === selectedCategory.id) {
+                    if (service.groupId) {
+                        const group = serviceGroups.find((item) => item.id === service.groupId);
+                        if (lastGroupId) {
+                            if (lastGroupId === group.id) {
+                                rowToRender += `<option value="${service.id}">${service.name}</option>`;
+                            } else {
+                                rowToRender += `
+                                </optgroup>
+                                <optgroup label="${group.name}">
+                                <option value="${service.id}">${service.name}</option>`;
+                                lastGroupId = group.id;
+                            }
+                        } else {
+                            rowToRender += `
+                            <optgroup label="${group.name}">
+                            <option value="${service.id}">${service.name}</option>`;
+                            lastGroupId = group.id;
+                        }
+                    } else {
+                        if (lastGroupId) {
+                            rowToRender += `
+                            </optgroup>
+                            <option value="${service.id}">${service.name}</option>`;
+                            lastGroupId = null;
+                        } else {
+                            rowToRender += `<option value="${service.id}">${service.name}</option>`;
+                        }
+                    }
+                }
+            }
         };
-        console.log(value)
+        $contentSelect.append(rowToRender);
+        $contentSelect.val(formData.content);
     }
 
-    function saveForm(event) {
-        console.log(formData);
+    function fillFormValues() {}
+
+    function formInputHandler({ target }) {
+        formData[target.name] = target;
+        console.log(formData)
+    }
+
+    function formBtnHandler(event) {
+        console.log($visibilitySelect.val())
+        event.preventDefault();
     }
 
 
@@ -201,9 +384,10 @@ $(document).ready(() => {
     $search.on('input', searchHandler);
     $clearSearchBtn.on('click', clearSearch);
     $tbody.on('click', 'tr', rowClickHandler);
-    $form.on('input', formInputHandler);
-
-    $saveBtn.on('click', saveForm);
+    $form
+        .on('input', formInputHandler)
+        .on('change', 'select', formInputHandler)
+        .on('click', 'button', formBtnHandler);
 
     // Init
     init();
